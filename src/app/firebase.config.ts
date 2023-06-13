d9859a6616d3761764f303a0746c6caa46a0f86b
yokesh-k <122250682+yokesh-k@users.noreply.github.com>
2023-06-13 21:42:56 +0530

    Add files via upload

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+
+
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDsEfGj-dWjqtman85VBNT5C7kDnYRA3yQ",
+  authDomain: "angular-ecommerceapp.firebaseapp.com",
+  projectId: "angular-ecommerceapp",
+  storageBucket: "angular-ecommerceapp.appspot.com",
+  messagingSenderId: "536354555816",
+  appId: "1:536354555816:web:d7738e592d4bd4c805bf9d"
+};
+
